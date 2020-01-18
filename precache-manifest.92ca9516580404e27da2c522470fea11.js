c931b9654e9c2705934fd65d6f64d81cb6c95701
Okoro Nnamdi <Nnasp4christ@yahoo.com>
2020-01-18 11:31:54 +0100

    Updates

---
+++ precache-manifest.92ca9516580404e27da2c522470fea11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b684e2d2b0301020b41febf3c5b9012",
+    "url": "/React-on-simplecalculator-app-in-github/index.html"
+  },
+  {
+    "revision": "26c6e3464a0687b9048d",
+    "url": "/React-on-simplecalculator-app-in-github/static/css/main.86c43490.chunk.css"
+  },
+  {
+    "revision": "05ee44fb054f392cc1e8",
+    "url": "/React-on-simplecalculator-app-in-github/static/js/2.15c57baa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-on-simplecalculator-app-in-github/static/js/2.15c57baa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26c6e3464a0687b9048d",
+    "url": "/React-on-simplecalculator-app-in-github/static/js/main.e39315d0.chunk.js"
+  },
+  {
+    "revision": "cd61e6320c378fff9771",
+    "url": "/React-on-simplecalculator-app-in-github/static/js/runtime-main.08e391ec.js"
+  }
+]);
